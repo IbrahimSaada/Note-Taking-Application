290e08e8df8ebc298d6b99f1bdf151932f7d78ce
Battomi <Battomi@DESKTOP-8OSKBF8>
2023-08-10 13:51:22 +0300

    Add project files.

---
+++ Note-Taking Application/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Note_Taking_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_new = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.txt_title = new System.Windows.Forms.TextBox();
+            this.txt_message = new System.Windows.Forms.TextBox();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(285, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 225);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btn_new
+            // 
+            this.btn_new.Location = new System.Drawing.Point(55, 356);
+            this.btn_new.Name = "btn_new";
+            this.btn_new.Size = new System.Drawing.Size(86, 23);
+            this.btn_new.TabIndex = 3;
+            this.btn_new.Text = "New";
+            this.btn_new.UseVisualStyleBackColor = true;
+            this.btn_new.Click += new System.EventHandler(this.btn_new_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(185, 356);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(86, 23);
+            this.btn_save.TabIndex = 4;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(304, 356);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(86, 23);
+            this.btn_update.TabIndex = 5;
+            this.btn_update.Text = "Edit";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(419, 356);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(86, 23);
+            this.btn_delete.TabIndex = 6;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // txt_title
+            // 
+            this.txt_title.Location = new System.Drawing.Point(73, 39);
+            this.txt_title.Name = "txt_title";
+            this.txt_title.Size = new System.Drawing.Size(188, 20);
+            this.txt_title.TabIndex = 7;
+            this.txt_title.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_message
+            // 
+            this.txt_message.Location = new System.Drawing.Point(73, 88);
+            this.txt_message.Multiline = true;
+            this.txt_message.Name = "txt_message";
+            this.txt_message.Size = new System.Drawing.Size(188, 176);
+            this.txt_message.TabIndex = 8;
+            this.txt_message.TextChanged += new System.EventHandler(this.txt_message_TextChanged);
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(233, 283);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(102, 36);
+            this.btn_read.TabIndex = 9;
+            this.btn_read.Text = "Read";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "My Notes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 414);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.txt_message);
+            this.Controls.Add(this.txt_title);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_new);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Notes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_new;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.TextBox txt_title;
+        private System.Windows.Forms.TextBox txt_message;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Note-Taking Application/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Note_Taking_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+    }
+
+        private void txt_message_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectingString = "Data Source=DESKTOP-8OSKBF8\\SQLEXPRESS;Initial Catalog=\"Note taking db\";Integrated Security=True";
+                using (SqlConnection conn = new SqlConnection(connectingString))
+                {
+                    conn.Open();
+                    string title = txt_title.Text;
+                    string message = txt_message.Text;
+                    string insertQuery = "INSERT INTO NoteTable (NoteTitle, NoteMessage) VALUES (@NoteTitle, @NoteMessage)";
+
+                    using (SqlCommand command = new SqlCommand(insertQuery, conn))
+                    {
+                        command.Parameters.AddWithValue("@NoteTitle", title);
+                        command.Parameters.AddWithValue("@NoteMessage", message);
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Added successfully");
+                        listBox1.Items.Add(title);
+                        txt_message.Clear();
+                        txt_title.Clear(); 
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+            }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectingString = "Data Source=DESKTOP-8OSKBF8\\SQLEXPRESS;Initial Catalog=\"Note taking db\";Integrated Security=True";
+                using (SqlConnection conn1 = new SqlConnection(connectingString))
+                {
+                    conn1.Open();
+                    string title = listBox1.SelectedItem.ToString();
+                    string SelectQuery = "SELECT NoteTitle, NoteMessage FROM NoteTable WHERE NoteTitle = @Title";
+
+                    using (SqlCommand command = new SqlCommand(SelectQuery, conn1))
+                    {
+                        command.Parameters.AddWithValue("@Title", title);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string noteTitle = reader["NoteTitle"].ToString();
+                                string noteMessage = reader["NoteMessage"].ToString();
+
+                                txt_title.Text = noteTitle;
+                                txt_message.Text = noteMessage;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void btn_new_Click(object sender, EventArgs e)
+        {
+            txt_title.Clear();
+            txt_message.Clear();
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectingString = "Data Source=DESKTOP-8OSKBF8\\SQLEXPRESS;Initial Catalog=\"Note taking db\";Integrated Security=True";
+                using (SqlConnection conn2 = new SqlConnection(connectingString))
+                {
+                    conn2.Open();
+                    string title = listBox1.SelectedItem.ToString();
+
+                    string DeleteQuery = "DELETE NoteTable WHERE NoteTitle = @Title";
+
+                    using (SqlCommand command = new SqlCommand(DeleteQuery, conn2))
+                    {
+                        command.Parameters.AddWithValue("@Title", title);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Record deleted successfully.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No record found to delete.");
+                        }
+                        listBox1.Items.Remove(title);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectingString = "Data Source=DESKTOP-8OSKBF8\\SQLEXPRESS;Initial Catalog=\"Note taking db\";Integrated Security=True";
+                using (SqlConnection conn3 = new SqlConnection(connectingString))
+                {
+                    conn3.Open();
+                    string oldTitle = listBox1.SelectedItem.ToString();
+                    string newTitle = txt_title.Text;
+                    string newMessage = txt_message.Text;
+
+                    string UpdateQuery = "UPDATE NoteTable SET NoteTitle = @NewTitle, NoteMessage = @NewMessage WHERE NoteTitle = @OldTitle";
+
+                    using (SqlCommand command = new SqlCommand(UpdateQuery, conn3))
+                    {
+                        command.Parameters.AddWithValue("@OldTitle", oldTitle);
+                        command.Parameters.AddWithValue("@NewTitle", newTitle);
+                        command.Parameters.AddWithValue("@NewMessage", newMessage);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Record updated successfully.");
+                            listBox1.Items.Remove(oldTitle);
+                            listBox1.Items.Add(newTitle);
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("No record found to update.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+    }
+    }
+    
